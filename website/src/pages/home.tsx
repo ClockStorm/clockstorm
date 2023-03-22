d752da07bd0fbc8087bf8739c5de72ffcaf04a14
Anthony Iacono <anthonyiacono@gmail.com>
2023-03-22 03:17:51 -0400

    Add a user guide page to the website
    This commit adds a user guide at a permanent location to help users get started.
    
    Co-authored-by: Anthony Iacono <anthonyiacono@gmail.com>
    Co-authored-by: Aidan McArthur <aidan.b.mcarthur@gmail.com>

--- website/src/pages/home.tsx
+++ website/src/pages/home.tsx
@@ -6,8 +6,16 @@ export const HomePage: React.FC = () => {
   return (
     <div id='page'>
       <p>The extension for Google Chrome that helps users remember to fill out their time cards.</p>
-      <div id='download'>
-        <Link to='/download'>Add to Chrome</Link>
+      <div id='links'>
+        <a
+          href='https://chrome.google.com/webstore/detail/clockstorm/jhkmhbncjplkgpdfkjjdjimhooffjbpl'
+          className='download'
+        >
+          Add to Chrome
+        </a>
+        <Link to='/guide' className='user-guide'>
+          User Guide
+        </Link>
       </div>
     </div>
   )
